d8fb11ff8b44eb0e9bfeea90ed3543de48e62289
Anastasiya Pugach <pugachas@gmail.com>
2021-03-31 18:01:38 +0300

    Убрана константа максимального числа

--- StringGamers/src/Main.java
+++ StringGamers/src/Main.java
@@ -5,29 +5,29 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        final int MAX_VALUE = 16;
         String[] info = {"Ivan 5", "Petr 3", "Alex 10", "Petr 8", "Ivan 6", "Alex 5", "Ivan 1", "Petr 5", "Alex 1"};
-        System.out.println(getWinner(info, MAX_VALUE));
+        System.out.println(getWinner(info));
     }
 
-    public static String getWinner(String[] playersInfo, int max_val){
+    public static String getWinner(String[] playersInfo){
         String result = "";
         HashMap<String, Integer> gamers = new HashMap<>();
         Pattern pattern = Pattern.compile("([a-zA-Z]+) ([0-9]+)");
+        int maxValue = 0;
+
         for(String player : playersInfo){
             Matcher matcher = pattern.matcher(player);
             if(matcher.find()){
                 String playerName = matcher.group(1);
                 int playerVal = Integer.parseInt(matcher.group(2));
                 if (gamers.containsKey(playerName)){
-                    int count = gamers.get(playerName) + playerVal;
-                    if(count >= max_val){
-                        result = playerName;
-                        break;
-                    }
-                    gamers.put(playerName, count);
-                } else {
-                    gamers.put(playerName, playerVal);
+                    playerVal += gamers.get(playerName);
+                }
+                gamers.put(playerName, playerVal);
+                //Смотрим победителя по очкам на данный момент
+                if(playerVal > maxValue){
+                    maxValue = playerVal;
+                    result = playerName;
                 }
             }
             else {
